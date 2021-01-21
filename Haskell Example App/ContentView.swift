7d7b848147f32770172aabd2fff785a5af4d818a
Devin Lehmacher <lehmacdj@gmail.com>
2021-01-20 22:39:02 -0800

    Initial Commit

---
+++ Haskell Example App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haskell Example App
+//
+//  Created by Devin Lehmacher on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Haskell Example App/Haskell_Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haskell_Example_AppApp.swift
+//  Haskell Example App
+//
+//  Created by Devin Lehmacher on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Haskell_Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
