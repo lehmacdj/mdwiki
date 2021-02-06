61dc700b1c2e35b85901b190ca1f6b0bc582a17c
Devin Lehmacher <lehmacdj@gmail.com>
2021-02-06 00:06:59 -0800

    finish implementation of most parsing things

--- hs-src/src/Commonmark/Pandoc/C.hs
+++ hs-src/src/Commonmark/Pandoc/C.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- {-# OPTIONS_GHC -fllvm #-}
 
@@ -12,12 +15,16 @@ import Control.Monad
 import Data.Aeson (ToJSON (..))
 import qualified Data.Aeson as Json
 import Data.Attoparsec.ByteString.Char8
+import qualified Data.Attoparsec.Text as APT
 import Data.Bifunctor (first, second)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 as B
 import Data.ByteString.Internal as BI
 import qualified Data.ByteString.Lazy as BL
-import Data.Text
+import Data.Functor
+import Data.List (lookup)
+import Data.Text as T
+import Data.Text (Text)
 import qualified Data.Text.Encoding as E
 import qualified Data.Text.Encoding.Error as E
 import qualified Data.Yaml.Aeson as Yaml
@@ -26,7 +33,8 @@ import Foreign.C (CString, newCString)
 import GHC.Ptr (Ptr (..))
 import Text.Pandoc.Builder (Blocks)
 import qualified Text.Pandoc.Builder as PB
-import Text.Pandoc.Definition (Block, Meta (..), Pandoc (..))
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk
 
 -- foreign export ccall "try_parse_commonmark" try_parse_commonmark :: CString -> Ptr CString -> IO Bool
 
@@ -57,8 +65,6 @@ try_parse_commonmark_json_api input outStrPtr = do
       poke outStrPtr outStr
       pure False
 
-{- HLint: ignore "Use camelCase" -}
-
 decodeUtf8 :: ByteString -> Text
 decodeUtf8 = E.decodeUtf8With E.lenientDecode
 
@@ -66,23 +72,29 @@ decodeUtf8 = E.decodeUtf8With E.lenientDecode
 -- the Int in 1 <= WantDash <= 3 at all times and represents the number of
 -- dashes that are still expected
 data ScanState
-  = WantDash !Int
-  | WantNewline
+  = WantStartNewline
+  | WantDash !Int
+  | WantFinalNewline
 
 -- | returns the frontmatter to be parsed as YAML + an offset in newlines that
 -- were parsed.
 parseFrontmatter :: Parser (ByteString, Int)
 parseFrontmatter = do
   _ <- string "---\n"
-  let doScan (WantDash n) '-'
-        | n == 1 = Just WantNewline
+  let doScan WantStartNewline '\n' = Just $ WantDash 3
+      doScan (WantDash n) '-'
+        | n == 1 = Just WantFinalNewline
         | n == 2 || n == 3 = Just $ WantDash (n - 1)
         | otherwise = error "invalid state"
-      doScan WantNewline '\n' = Nothing
-      doScan _ _ = Just $ WantDash 3
-  frontmatter <- scan (WantDash 3) doScan
+      doScan WantFinalNewline '\n' = Nothing
+      doScan _ _ = Just WantStartNewline
+  frontmatter <- scan WantStartNewline doScan
   let newlinesInYaml = countCharsInStr '\n' frontmatter
-  pure (frontmatter, newlinesInYaml + 2)
+  -- @optimization: it might be possible to avoid concattenating the "\n" at the
+  -- end of the string here. But its not worth messing with this unless we
+  -- really need to squeeze performance out of this + probably after
+  -- benchmarking
+  pure (frontmatter <> "\n", newlinesInYaml + 3)
   where
     countCharsInStr c = B.length . B.filter (== c)
 
@@ -91,17 +103,20 @@ parseFrontmatter = do
 -- returns the header which should be parsed as YAML, an offset in lines that
 -- should be used to adjust the line numbers of the markdown in Left or just
 -- the markdown as text in Right
-parseMarkdownDocument :: Parser (Either (ByteString, Int, Text) Text)
-parseMarkdownDocument = do
-  _ <- optional byteOrderMark
-  mFrontmatter <- optional parseFrontmatter
-  let mkResult markdown = case mFrontmatter of
-        Just (frontmatter, offset) -> Left (frontmatter, offset, markdown)
-        Nothing -> Right markdown
-  markdownText <- decodeUtf8 <$> takeByteString
-  pure $ mkResult markdownText
+separateFrontmatterAndBody ::
+  ByteString -> Either MyParseError (Either (ByteString, Int, Text) Text)
+separateFrontmatterAndBody = first SeparationParseError . parseOnly parser
   where
     byteOrderMark = "\xef\xbb\xbf"
+    parser =
+      do
+        _ <- optional byteOrderMark
+        mFrontmatter <- optional parseFrontmatter
+        let mkResult markdown = case mFrontmatter of
+              Just (frontmatter, offset) -> Left (frontmatter, offset, markdown)
+              Nothing -> Right markdown
+        markdownText <- decodeUtf8 <$> takeByteString
+        pure $ mkResult markdownText
 
 -- | Copies 'ByteString' to newly allocated 'CString'. The result must be
 -- explicitly freed using 'free' or 'finalizerFree'.
@@ -114,7 +129,14 @@ newCStringFromBS (BI.PS fp o l) = do
     pokeByteOff buf l (0 :: Word8)
     return $ castPtr buf
 
-data MyParseError = ParseError ParseError | UnspecifiedError
+data MyParseError
+  = SeparationParseError String
+  | JsonParseError ParseError
+  | -- | Takes first a string representing the attr in which the source range
+    -- failed to parse then the error message produced by Attoparsec
+    SourceRangeParseError String String
+  | CommonmarkParseError ParseError
+  | UnspecifiedError
 
 instance ToJSON MyParseError where
   toJSON = undefined
@@ -126,32 +148,78 @@ render = undefined
 renderError :: MyParseError -> ByteString
 renderError = undefined
 
--- | TODO: use Pandoc to separate header from markdown body and then parse both
--- separately using their own parser function followed by returning the
--- complete 'ParsedCommonmark'.
---
--- * the source annotations will need to be adjusted for the size of the yaml header
--- * the yaml header should be optional, i.e. parsing should not fail if it
--- is completely missing
--- * title of the markdown note (the first H1 in the document) should also be
--- added to the metadata as the title, unless there is already a title field
--- there
+tshow :: Show a => a -> Text
+tshow = T.pack . show
+
+data SimpleSourceRange = SimpleSourceRange
+  { sourceRangeFilename :: Text,
+    sourceRangeLineStart :: Int,
+    sourceRangeColumnStart :: Int,
+    sourceRangeLineEnd :: Int,
+    sourceRangeColumnEnd :: Int
+  }
+  deriving (Eq)
+
+renderSourceRange :: SimpleSourceRange -> Text
+renderSourceRange (SimpleSourceRange fn ls cs le ce) =
+  fn <> "@" <> tshow ls <> ":" <> tshow cs <> "-" <> tshow le <> ":" <> tshow ce
+
+-- | takes a string representation of the span that this source range, and the
+-- Text representing the source range and parses it, or produces an error
+parseSourceRange :: String -> Text -> Either MyParseError SimpleSourceRange
+parseSourceRange spanStr =
+  first (SourceRangeParseError spanStr) . APT.parseOnly sourceRangeP
+  where
+    sourceRangeP =
+      SimpleSourceRange
+        <$> APT.takeWhile (/= '@')
+        <*> (APT.decimal <* lcSep)
+        <*> (APT.decimal <* seSep)
+        <*> (APT.decimal <* lcSep)
+        <*> (APT.decimal <* APT.endOfInput)
+    lcSep = APT.string ":" $> ()
+    seSep = APT.string "-" $> ()
+
+-- | add a number of lines to the start and end of the range
+plusLines :: SimpleSourceRange -> Int -> SimpleSourceRange
+plusLines (SimpleSourceRange fn ls cs le ce) offset =
+  SimpleSourceRange fn (ls + offset) cs (le + offset) ce
+
+attrSetKey :: Text -> Text -> Attr -> Attr
+attrSetKey = undefined
+
 parseDocument :: ByteString -> Either MyParseError Pandoc
-parseDocument = undefined
+parseDocument input = do
+  separated <- separateFrontmatterAndBody input
+  let makeMeta (frontmatter, offset, body) =
+        (,offset,body) <$> yamlHeaderToMeta frontmatter
+  (meta, offset, body) <- either makeMeta (Right . (mempty,0,)) separated
+  blocks <- commonmarkToPandocBlocks body
+  let offsetDataPos = \case
+        Span attr@(_, _, lookup "data-pos" -> Just srText) contents -> do
+          sourceRange <- parseSourceRange (show attr) srText
+          let newSrText = renderSourceRange (sourceRange `plusLines` offset)
+              attr' = attrSetKey "data-pos" newSrText attr
+          pure $ Span attr' contents
+        x -> pure x
+  posAdjustedBlocks <- walkM offsetDataPos blocks
+  pure $ Pandoc meta posAdjustedBlocks
 
 -- | use YAML parsing library to parse YAML header into YAML fields
-yamlHeaderToMeta :: Text -> Either MyParseError Meta
-yamlHeaderToMeta = undefined
+-- TODO: actually parse YAML Frontmatter instead of just returning an empty
+-- meta block
+yamlHeaderToMeta :: ByteString -> Either MyParseError Meta
+yamlHeaderToMeta _ = Right mempty
 
-commonmarkToPandocJson ::
+commonmarkToPandocBlocks ::
   Text ->
   Either MyParseError [Block]
-commonmarkToPandocJson text =
+commonmarkToPandocBlocks text =
   case parseCommonmarkWith defaultSyntaxSpec (tokenize "source" text) of
     Just (Right ast) ->
       Right
         . PB.toList
         . unCm
         $ (ast :: Cm SourceRange Blocks)
-    Just (Left err) -> Left . ParseError $ err
+    Just (Left err) -> Left . CommonmarkParseError $ err
     Nothing -> Left UnspecifiedError
